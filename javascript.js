ee7cc7f11e66135ce1883cac2d7ff83878a7aa5e
Gaston Da Ruda Web Developer <82277891+gastondaruda@users.noreply.github.com>
2021-11-26 00:52:02 -0300

    Dom

--- javascript.js
+++ javascript.js
@@ -1,255 +1,128 @@
 
-alert("Hola Facundo, te invito a hacer click en el desafío que quieras ver. Gracias.")
 
 
-/*
-let usuarioRegistrado = "Gastón"
-let password = 1234;
-let error = 0;
-
-function acceder(){
-for( let i = 1; i <= 3 ; i++ ) {
-
-    let usuario = prompt("Ingrese su usuario por favor");
-    let contraseña = prompt("Ingrese su contraseña por favor");
-
-    if (usuario == usuarioRegistrado && password == contraseña) {
-        alert("Bienvenido " + usuarioRegistrado)
-        break;
-
-    } 
-    else {
-        alert("Contraseña incorrecta. Intento nº" + i);
-        error = error + 1;
-        if(error == 3) {
-            alert("Chau tarjeta");
-            break;
-        }
+let juegos = [
     
-    
-        
-        
-        
-    }
-    
-}
-}
-
-document.getElementById("desafio3").onclick = function () {
-    acceder();
-}
-
-*/
-
-
-//----------------WHILE-------------------------
-
-
-/*
-function bucle() {
-    let Usuario = prompt("Ingrese su nombre o ESC para salir")
-while ( Usuario != "ESC") {
-    console.log("Hola " + Usuario);
-    Usuario = prompt("Ingrese su nombre o ESC para salir"); 
-}
-}
-
-
-
-document.getElementById("desafioWhile").onclick = function () {
-    bucle();
-}
-
-
-
-
-
-*/
-// DESAFIO FUNCIONES
-
-//push en https://github.com/gastondaruda/javascript
-
-
-
-function sumar(){
-    let a = parseInt(prompt("Ingresa el primer precio"));
-    let b = parseInt(prompt("Ingresa el segundo precio"));
-    let c = a + b;
-    console.log("la suma de los dos productos que elegiste es de $ " + c)
-    return c
-}
-
-
-
-let suma = sumar();
-
-document.getElementById("suma").onclick = function () {
-    sumar(suma);
-    
-}
-
-
-//---------------------
-/*
-function iva(suma){
-    let iva = parseFloat(suma * 0.21);
-    console.log("El iva ahora es de " + iva)
-    return iva;
-}
-
-let precioIva = iva(suma);
-
-document.getElementById("iva").onclick = function () {
-    iva(suma);
-    }
-
-let total = precioIva + suma;
-
-*/
-//--------------------
-/*
-function precioTotal(){
-    
-    console.log("El resultado final con el iva sumado es de $" + total)
-    return 
-}
-
-let precioTotal = total();
-
-
-document.getElementById("total").onclick = function () {
-    precioTotal(total);
-    }
-
-
-    document.write(precioTotal),
-
-*/
-
-/*
-function todo(){
-    sumar();
-    iva(c);
-    total(c, precioIva);
-}
-
-
-
-document.getElementById("todo").onclick = function () {
-    todo();
-    }
-
-*/
-
-//DESAFIO OBJETOS
-
-
-//CONSTRUCTORES Y PROPIEDADES
-//clases
-class videogame{
-    constructor ( nombre , empresa, year, precio){
-    this.nombre = nombre;
-    this.empresa = empresa;
-    this.year = year;
-    this.precio = precio
-
-}
-
-monto(){
-    alert("El precio es de " + this.nombre + " es de " + this.precio)
-}
-}
-
-
-
-let GodOfWar = new videogame("God of War" , "Santa monica", 2018 , "$1500")
-
-document.getElementById("gow").onclick = function () {
-    console.log(GodOfWar);
-    GodOfWar.monto();
-}
-
-
-
-
-let ReIV = new videogame ( "Resident Evil 4", "Capcom" , 2005, "$1200");
-document.getElementById("re").onclick = function () {
-    console.log(ReIV);
-    ReIV.monto();
-}
-    
-let uncharted = new videogame("Uncharted 4: El deselace de un ladrón", "Naughhty Dog" , 2026, "$800")
-
-    document.getElementById("uncharted").onclick = function () {
-        console.log(uncharted);
-        uncharted.monto();
-    }
-
-
-let ReIII = new videogame ("Resident Evil 3 Remake" , "Capcom" , 2020, "$1600")
-document.getElementById("re3").onclick = function () {
-    console.log(ReIII);
-    ReIII.monto();
-}
-
-
-let MetalGearSolid = new videogame("Metal Gear Solid: Ground zeroes" , "Kojima Productions" , 2014, "$850")
-document.getElementById("mgs").onclick = function () {
-    console.log(MetalGearSolid);
-    MetalGearSolid.monto();
-}
-
-
-let Dbz = new videogame("Dragon Ball Fighters" , "Arc System Works" , 2017, "$1000")
-document.getElementById("dbz").onclick = function () {
-    console.log(Dbz);
-    Dbz.monto();
-}
-
-
-//----------- Objetos dentro de un Array
-let games = [
-    {
-        nombre: "Uncharted 3",
-        empresa: "naughty Dogs",
-        year: 2010,
-        precio: 900
-    },
-
-    {
-        nombre: "Fifa",
-        empresa: "Fifa",
-        year: 2021,
-        precio: 2300
-    }
-    
-    
+    {nombre:"God Of War" , genero: "Acción"},
+    {nombre:"Need for Speed Payback" , genero: "Autos"},
+    {nombre:"Outlast" , genero: "Survival Horror"},
+    {nombre:"Resident Evil4" , genero: "Survival Horror"},
+    {nombre:"Resident Evil 3", genero: "Survival Horror"},
+    {nombre:"Forza Horizon 4" , genero: "Autos"},
+    {nombre: "Horizon Zero Dawn", genero: "Ación"},
+    {nombre:"Uncharted 4" , genero: "Acción"},
+    {nombre: "Metal Gear Solid V", genero:"Acción"},
+    {nombre: "Dragon Ball Fighters Z", genero:"Acción"},
+    {nombre:"Fifa" , genero: "Deporte"},
+    {nombre:"Pes" , genero: "Deporte"},
+    {nombre:"Guitar Hero 5" , genero: "Música"}
 ]
 
-console.log(games)
 
-
-//Ordenar un Array-----------------------
-let nombreJuegos = [
-    {nombre: "God of war"},
-    {nombre:"Limbo"},
-    {nombre:"Call of Duty"},
-    {nombre:"Warzone"},
-    {nombre:"Battlefield"},
-    {nombre:"Outlast"},
-    {nombre:"Tetris"},
-]
-
-nombreJuegos.sort((a , b) => {
-    if(a.nombre < b.nombre){
-        return -1;
-    }
-    if ( a.nombre > b.nombre){
-        return 1;
-    }
-    return 0;
+let JuegosDeACcion = juegos.filter(function(juegos){
+    return juegos.genero === "Acción";
 });
 
-console.log(nombreJuegos);
+let JuegosDeSurvival = juegos.filter(function(juegos){
+    return juegos.genero === "Survival Horror";
+});
+
+let JuegosDeAutos = juegos.filter(function(juegos){
+    return juegos.genero === "Autos";
+});
+
+let JuegosDeDeporte = juegos.filter(function(juegos){
+    return juegos.genero === "Deporte";
+});
+
+function accion(){
+    console.log(JuegosDeACcion);
+}
+function deporte(){
+    console.log(JuegosDeDeporte);
+}
+
+function autos(){
+    console.log(JuegosDeAutos);
+}
+
+function survival(){
+    console.log(JuegosDeSurvival);
+}
+
+$(document).ready(function(){
+    //AGREGANDO CLASE ACTIVE AL PRIMER ENLACE
+    $('.genero__items .genero__item[category="all"]').addClass('btn-active');
+
+    //-----------------------------------
+
+    $('.genero__item').click(function(){
+        let catProduct = $(this).attr('category');
+
+        //ocultando productos
+        $('.container__box').hide();
+
+        //------
+
+        $('.container__box[category="'+catProduct+'"]').show();
 
 
+    });
+
+    $('.genero__item[category="all"]').click(function(){
+        $('.container__box').show();
+    })
+
+
+});
+
+
+
+// --------------lista de deseados
+
+
+let valor =  document.getElementsByClassName("elemento");
+function listaDeDeseados(valor){
+
+    
+    let lista = document.getElementById("lista");
+
+    
+    let nuevoLi = document.createElement("li");
+
+    nuevoLi.innerHTML = valor;
+    
+    lista.appendChild( nuevoLi);
+   
+    
+    
+
+}
+
+
+/*
+function listaCompras(){
+
+    let valor =  document.getElementById("elemento");
+    let lista = document.getElementById("lista");
+
+    
+    let nuevoLi = document.createElement("li");
+
+    nuevoLi.innerHTML = `${valor.value}`;
+    
+    lista.appendChild( nuevoLi);
+
+}
+*/
+
+
+
+
+
+function borrarElemento(){
+
+    let lista = document.getElementById("lista");
+    lista.removeChild(lista.lastChild);
+
+}
